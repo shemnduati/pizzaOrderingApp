9e052fbec9ace0e7f0d6520392eb607b125f0b24
Shem Nduati <nduatishem@gmail.com>
2023-12-09 08:08:20 +0300

    working with the item card

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -4,11 +4,11 @@ export default function HomeMenu(){
     return(
         <section>
             <div>
-                <div className="relative">
-                    <div className="h-48 w-48 absolute left-0">
+                <div className="absolute left-0 right-0 h-full w-full justify-start">
+                    <div className="h-48 w-48 absolute -top-[70] -left-12 -z-10">
                         <Image src={'/sallad1.png'} layout={'fill'} odjectFit={'contain'} alt={'sallad'} />
                     </div>
-                    <div className="h-48 w-48 absolute right-0">
+                    <div className="h-48 w-48 absolute -top-[100] -right-12 -z-10">
                         <Image src={'/sallad2.png'} layout={'fill'} odjectFit={'contain'} alt={'sallad'} />
                     </div>
                 </div>
@@ -17,6 +17,19 @@ export default function HomeMenu(){
                 Check out
             </h3>
             <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                <div className="bg-gray-300 rounded-lg p-4 text-center">
+                    <img src="/pizza.png" alt="Pizza"></img>
+                    <h4 className="font-semibold text-xl my-2">Pepporoni Pizza</h4>
+                    <p className="text-gray-500 text-sm">
+                        Indulge in a symphony of flavors with our mouthwatering pizzas, expertly crafted to perfection
+                         and now available on our online food platform!
+                    </p>
+                    <button className="bg-primary text-white rounded-full px-4 py-2">
+                        Add to cart $12
+                    </button>
+                </div>
             </div>
             </div>
             
